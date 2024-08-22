e6527d0f93590606b707ca771e2e729a8daa6cf3
Febin Johnson <febinnj1996@gmail.com>
2024-08-22 11:26:52 +0530

    added gen assets

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,40 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsSvgGen {
+  const $AssetsSvgGen();
+
+  /// Directory path: assets/svg/home_screen
+  $AssetsSvgHomeScreenGen get homeScreen => const $AssetsSvgHomeScreenGen();
+}
+
+class $AssetsSvgHomeScreenGen {
+  const $AssetsSvgHomeScreenGen();
+
+  /// File path: assets/svg/home_screen/classroom.svg
+  String get classroom => 'assets/svg/home_screen/classroom.svg';
+
+  /// File path: assets/svg/home_screen/registration.svg
+  String get registration => 'assets/svg/home_screen/registration.svg';
+
+  /// File path: assets/svg/home_screen/student.svg
+  String get student => 'assets/svg/home_screen/student.svg';
+
+  /// File path: assets/svg/home_screen/subject.svg
+  String get subject => 'assets/svg/home_screen/subject.svg';
+
+  /// List of all assets
+  List<String> get values => [classroom, registration, student, subject];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsSvgGen svg = $AssetsSvgGen();
+}
