73b46bc86fd8d8cabcf42d1150984871a8b9509f
Febin Johnson <febinnj1996@gmail.com>
2024-08-24 05:03:10 +0530

    added subject screen and subject detail screen (#5)

---
+++ lib/presentation/subjects/bloc/subject_bloc.freezed.dart
@@ -0,0 +1,469 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'subject_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$SubjectEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() onLoad,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? onLoad,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? onLoad,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_OnLoad value) onLoad,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_OnLoad value)? onLoad,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_OnLoad value)? onLoad,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SubjectEventCopyWith<$Res> {
+  factory $SubjectEventCopyWith(
+          SubjectEvent value, $Res Function(SubjectEvent) then) =
+      _$SubjectEventCopyWithImpl<$Res, SubjectEvent>;
+}
+
+/// @nodoc
+class _$SubjectEventCopyWithImpl<$Res, $Val extends SubjectEvent>
+    implements $SubjectEventCopyWith<$Res> {
+  _$SubjectEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$OnLoadImplCopyWith<$Res> {
+  factory _$$OnLoadImplCopyWith(
+          _$OnLoadImpl value, $Res Function(_$OnLoadImpl) then) =
+      __$$OnLoadImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$OnLoadImplCopyWithImpl<$Res>
+    extends _$SubjectEventCopyWithImpl<$Res, _$OnLoadImpl>
+    implements _$$OnLoadImplCopyWith<$Res> {
+  __$$OnLoadImplCopyWithImpl(
+      _$OnLoadImpl _value, $Res Function(_$OnLoadImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$OnLoadImpl implements _OnLoad {
+  const _$OnLoadImpl();
+
+  @override
+  String toString() {
+    return 'SubjectEvent.onLoad()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$OnLoadImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() onLoad,
+  }) {
+    return onLoad();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? onLoad,
+  }) {
+    return onLoad?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? onLoad,
+    required TResult orElse(),
+  }) {
+    if (onLoad != null) {
+      return onLoad();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_OnLoad value) onLoad,
+  }) {
+    return onLoad(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_OnLoad value)? onLoad,
+  }) {
+    return onLoad?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_OnLoad value)? onLoad,
+    required TResult orElse(),
+  }) {
+    if (onLoad != null) {
+      return onLoad(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _OnLoad implements SubjectEvent {
+  const factory _OnLoad() = _$OnLoadImpl;
+}
+
+/// @nodoc
+mixin _$SubjectState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<SubjectModel> subjectList) onLoadState,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<SubjectModel> subjectList)? onLoadState,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<SubjectModel> subjectList)? onLoadState,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_OnLoadState value) onLoadState,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_OnLoadState value)? onLoadState,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_OnLoadState value)? onLoadState,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $SubjectStateCopyWith<$Res> {
+  factory $SubjectStateCopyWith(
+          SubjectState value, $Res Function(SubjectState) then) =
+      _$SubjectStateCopyWithImpl<$Res, SubjectState>;
+}
+
+/// @nodoc
+class _$SubjectStateCopyWithImpl<$Res, $Val extends SubjectState>
+    implements $SubjectStateCopyWith<$Res> {
+  _$SubjectStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$InitialImplCopyWith<$Res> {
+  factory _$$InitialImplCopyWith(
+          _$InitialImpl value, $Res Function(_$InitialImpl) then) =
+      __$$InitialImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitialImplCopyWithImpl<$Res>
+    extends _$SubjectStateCopyWithImpl<$Res, _$InitialImpl>
+    implements _$$InitialImplCopyWith<$Res> {
+  __$$InitialImplCopyWithImpl(
+      _$InitialImpl _value, $Res Function(_$InitialImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$InitialImpl implements _Initial {
+  const _$InitialImpl();
+
+  @override
+  String toString() {
+    return 'SubjectState.initial()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InitialImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<SubjectModel> subjectList) onLoadState,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<SubjectModel> subjectList)? onLoadState,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<SubjectModel> subjectList)? onLoadState,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_OnLoadState value) onLoadState,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_OnLoadState value)? onLoadState,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_OnLoadState value)? onLoadState,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initial implements SubjectState {
+  const factory _Initial() = _$InitialImpl;
+}
+
+/// @nodoc
+abstract class _$$OnLoadStateImplCopyWith<$Res> {
+  factory _$$OnLoadStateImplCopyWith(
+          _$OnLoadStateImpl value, $Res Function(_$OnLoadStateImpl) then) =
+      __$$OnLoadStateImplCopyWithImpl<$Res>;
+  @useResult
+  $Res call({List<SubjectModel> subjectList});
+}
+
+/// @nodoc
+class __$$OnLoadStateImplCopyWithImpl<$Res>
+    extends _$SubjectStateCopyWithImpl<$Res, _$OnLoadStateImpl>
+    implements _$$OnLoadStateImplCopyWith<$Res> {
+  __$$OnLoadStateImplCopyWithImpl(
+      _$OnLoadStateImpl _value, $Res Function(_$OnLoadStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? subjectList = null,
+  }) {
+    return _then(_$OnLoadStateImpl(
+      subjectList: null == subjectList
+          ? _value._subjectList
+          : subjectList // ignore: cast_nullable_to_non_nullable
+              as List<SubjectModel>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$OnLoadStateImpl implements _OnLoadState {
+  const _$OnLoadStateImpl({required final List<SubjectModel> subjectList})
+      : _subjectList = subjectList;
+
+  final List<SubjectModel> _subjectList;
+  @override
+  List<SubjectModel> get subjectList {
+    if (_subjectList is EqualUnmodifiableListView) return _subjectList;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_subjectList);
+  }
+
+  @override
+  String toString() {
+    return 'SubjectState.onLoadState(subjectList: $subjectList)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$OnLoadStateImpl &&
+            const DeepCollectionEquality()
+                .equals(other._subjectList, _subjectList));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, const DeepCollectionEquality().hash(_subjectList));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$OnLoadStateImplCopyWith<_$OnLoadStateImpl> get copyWith =>
+      __$$OnLoadStateImplCopyWithImpl<_$OnLoadStateImpl>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<SubjectModel> subjectList) onLoadState,
+  }) {
+    return onLoadState(subjectList);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<SubjectModel> subjectList)? onLoadState,
+  }) {
+    return onLoadState?.call(subjectList);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<SubjectModel> subjectList)? onLoadState,
+    required TResult orElse(),
+  }) {
+    if (onLoadState != null) {
+      return onLoadState(subjectList);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_OnLoadState value) onLoadState,
+  }) {
+    return onLoadState(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_OnLoadState value)? onLoadState,
+  }) {
+    return onLoadState?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_OnLoadState value)? onLoadState,
+    required TResult orElse(),
+  }) {
+    if (onLoadState != null) {
+      return onLoadState(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _OnLoadState implements SubjectState {
+  const factory _OnLoadState({required final List<SubjectModel> subjectList}) =
+      _$OnLoadStateImpl;
+
+  List<SubjectModel> get subjectList;
+  @JsonKey(ignore: true)
+  _$$OnLoadStateImplCopyWith<_$OnLoadStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
